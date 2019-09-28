080a6b10b26f990beee495728571601fd9480a9b
kshannon <k.shannon.ucsd@gmail.com>
2019-09-28 12:06:33 -0700

    simple shell script to create base config.ini

---
+++ src/config/create_config.sh
@@ -0,0 +1,9 @@
+cat > config.ini <<- "EOF"
+    [path]
+    s1_train_path = ''
+    s1_test_path = ''
+    df_path = ''
+    train_csv_path = '../training.csv'
+    validate_csv_path = '../validation.csv'
+    test_csv_path = '../testing.csv'
+EOF
