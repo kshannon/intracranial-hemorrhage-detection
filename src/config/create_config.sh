3b8437e0ad83be4e28b31185d2c01ac88af9d0ba
kshannon <k.shannon.ucsd@gmail.com>
2019-09-28 12:14:17 -0700

    clean up data injestion script

--- src/config/create_config.sh
+++ src/config/create_config.sh
@@ -3,7 +3,4 @@ cat > config.ini <<- "EOF"
     s1_train_path = ''
     s1_test_path = ''
     df_path = ''
-    train_csv_path = '../training.csv'
-    validate_csv_path = '../validation.csv'
-    test_csv_path = '../testing.csv'
 EOF
--- src/data_ingest.py
+++ src/data_ingest.py
@@ -4,8 +4,13 @@
 import sys
 import os
 import numpy as np
-import pydicom
+import pandas as pd
 import configparser
+import pydicom
+import PIL
+from PIL import Image
+import gdcm
+import cv2
 
 config = configparser.ConfigParser()
 config.read('./config.ini')
@@ -13,17 +18,34 @@ config.read('./config.ini')
 # stage 1 data
 train_data_path = config['path']['s1_train_path']
 test_data_path = config['path']['s1_test_path']
+df_path = config['path']['df_path']
 
-#TODO: add a window/level option in here
-def translate_dicom(filename, path=train_data_path, test=False):
+
+def translate_dicom(filename,path=train_data_path,apply_window=None,test=False):
     """
     Transform a medical DICOM file to a standardized pixel based array
+    Arguments:
+        filename {[type]} -- [description]
+        path {string} -- file path to data, set in config.ini
+        apply_window {[type]} -- [description]
+        test {bool} -- when true read from the test dicom directory
     """
     if test == True:
         path = test_data_path
 
+    if apply_window != None:
+        #TODO: take in here a tuple or dict for window/leveling and apply to dcmread function
+        apply_window = None
+
     img = np.array(pydicom.dcmread(path + filename).pixel_array, dtype=float).T
-    mean = img.mean()
-    std = img.std()
-    standardized_array = np.divide(np.subtract(img,mean),std)
+    standardized_array = np.divide(np.subtract(img,img.mean()),img.std())
     return standardized_array
+
+
+def load_df(df_path):
+    """[summary]
+    
+    Arguments:
+        df_path {[type]} -- [description]
+    """
+    pass
