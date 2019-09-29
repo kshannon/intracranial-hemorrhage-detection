c0f826f2308c31df2079e4477a2c2247c6a14485
kshannon <k.shannon.ucsd@gmail.com>
2019-09-29 10:08:36 -0700

    update shell script to write docker paths

--- src/config/create_config.sh
+++ src/config/create_config.sh
@@ -2,5 +2,9 @@ cat > config.ini <<- "EOF"
     [path]
     s1_train_path = ''
     s1_test_path = ''
-    df_path = ''
+    docker_train = ../../data/stage_1_train_images/
+    docker_test = ../../data/stage_1_test_images/
+    train_csv_path = training.csv
+    validate_csv_path = validation.csv
+    test_csv_path = testing.csv
 EOF
