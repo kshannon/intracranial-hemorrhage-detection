05b3aa51656aaf30e7788dd4706b233cfe421825
kshannon <k.shannon.ucsd@gmail.com>
2019-10-02 00:38:44 -0700

    clean up bad_actors script and added a script to list all sage 1 test folder dicom IDs in a scv incase we need them

--- eda/bad_actors.py
+++ eda/bad_actors.py
@@ -11,12 +11,12 @@ from tqdm.auto import tqdm
 tqdm.pandas()
 
 
-# TRAIN_CSV_PATH = '../src/training.csv'
+TRAIN_CSV_PATH = '../src/training.csv'
 VALIDATE_CSV_PATH = '../src/validation.csv'
-# TEST_CSV_PATH = '../src/testing.csv'
+TEST_CSV_PATH = '../src/testing.csv'
 
 train_data = '../../data/stage_1_train_images/'
-# test_data = '../../data/stage_1_test_images/'
+test_data = '../../data/stage_1_test_images/'
 
 
 def check_dicom(row, path=train_data):
@@ -35,15 +35,17 @@ def check_dicom(row, path=train_data):
         return False
     return True
 
+# Comment or Uncomment to process TRAINING data
+df_train = pd.read_csv(TRAIN_CSV_PATH)
+df_train['bad_actors'] = df_train.progress_apply(lambda x: check_dicom(x), axis=1)
+df_train.to_csv('../src/train_flagged.csv', index=False)
 
-# df_train = pd.read_csv(TRAIN_CSV_PATH)
-# df_train['bad_actors'] = df_train.progress_apply(lambda x: check_dicom(x), axis=1)
-# df_train.to_csv('../src/train_flagged.csv', index=False)
-
+# Comment or Uncomment to process VALIDATION data
 df_validate = pd.read_csv(VALIDATE_CSV_PATH)
 df_validate['bad_actors'] = df_validate.progress_apply(lambda x: check_dicom(x), axis=1)
 df_validate.to_csv('../src/validate_flagged.csv', index=False)
 
+# Comment or Uncomment to process TEST data
 # df_test = pd.read_csv(TEST_CSV_PATH)
 # df_test['bad_actors'] = df_test.progress_apply(lambda x: check_dicom(x),axis=1)
 # df_test.to_csv('../src/test_flagged.csv', index=False)
