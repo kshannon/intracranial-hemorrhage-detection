3d245ec5f609ac2f04f8e434811a3f6d6ee96a99
Tony Reina <g.anthony.reina@intel.com>
2019-10-03 19:42:40 -0700

    Reduced cores to 4

--- eda/bad_actors.py
+++ eda/bad_actors.py
@@ -40,9 +40,9 @@ def check_dicom(row, path=train_data):
         return False
 
     return True
-    
 
-def parallelize_dataframe(df, func, n_cores=8):
+
+def parallelize_dataframe(df, func, n_cores=4):
     df_split = np.array_split(df, n_cores)
     pool = Pool(n_cores)
     df = pd.concat(pool.map(func, df_split))
