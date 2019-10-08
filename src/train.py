9e62443699471cbc77b11eff27a7bf4a1b91c504
kshannon <k.shannon.ucsd@gmail.com>
2019-10-07 23:30:12 -0700

    add resize options to train script, plus resnet50, baseline/vgg16/resnet50 individual model definitions can be found in src/model_defs/

--- src/train.py
+++ src/train.py
@@ -4,6 +4,7 @@ import numpy as np
 import datetime
 import tensorflow as tf
 from tensorflow import keras as K
+# from keras_applications.resnet import ResNet50
 from tensorflow import ConfigProto
 from tensorflow import InteractiveSession
 from data_loader import DataGenerator
@@ -21,26 +22,33 @@ if parse_config.USING_RTX_20XX:
 MODEL_NAME = sys.argv[1]
 DATA_DIRECTORY = data_flow.TRAIN_DATA_PATH
 TRAIN_CSV = parse_config.VALIDATE_CSV
+# TRAIN_CSV = "./class_one_training.csv"
 VALIDATE_CSV = parse_config.VALIDATE_CSV
-BATCH_SIZE = 16
-EPOCHS = 2
-TRAINING_DATA = DataGenerator(csv_filename=TRAIN_CSV, data_path=DATA_DIRECTORY, batch_size=BATCH_SIZE)
-VALIDATION_DATA = DataGenerator(csv_filename=VALIDATE_CSV, data_path=DATA_DIRECTORY, batch_size=BATCH_SIZE)
+BATCH_SIZE = 1
+EPOCHS = 5
+RESIZE = (224,224) #comment out if not needed and erase param below
+DIMS = (224,224)
+TRAINING_DATA = DataGenerator(csv_filename=TRAIN_CSV, data_path=DATA_DIRECTORY, batch_size=BATCH_SIZE, resize=RESIZE, dims=DIMS)
+VALIDATION_DATA = DataGenerator(csv_filename=VALIDATE_CSV, data_path=DATA_DIRECTORY, batch_size=BATCH_SIZE, resize=RESIZE, dims=DIMS)
 
 
 # Saved models
 checkpoint = K.callbacks.ModelCheckpoint(os.path.join('../models/', sys.argv[1] + '.pb'), verbose=1, save_best_only=True)
                                                        
 # TensorBoard    
-tb_logs = K.callbacks.TensorBoard(log_dir = os.path.join('tensorboards/', sys.argv[1]), histogram_freq=1)
+tb_logs = K.callbacks.TensorBoard(log_dir = os.path.join('tensorboards/', sys.argv[1]))
 # log_dir="logs/fit/" + datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
 
 
-############## Model Definition goes here ##############
+############## Specific Model Definition STARTS here ##############
 num_chan_in = 2
-height = 512
-width = 512
+height = 224
+width = 224
 num_classes = 6
+bn_momentum = 0.99
+BATCH_SIZE = 64
+EPOCHS = 15 
+
 
 inputs = K.layers.Input([height, width, num_chan_in], name="DICOM")
 
@@ -48,39 +56,46 @@ params = dict(kernel_size=(3, 3), activation="relu",
                       padding="same",
                       kernel_initializer="he_uniform")
 
-convA = K.layers.Conv2D(name="convAa", filters=32, **params)(inputs)
-convA = K.layers.Conv2D(name="convAb", filters=32, **params)(convA)
-poolA = K.layers.MaxPooling2D(name="poolA", pool_size=(2, 2))(convA)
+img_1 = K.layers.BatchNormalization(momentum=bn_momentum)(inputs)
+img_1 = K.layers.Conv2D(32, **params)(img_1)
+img_1 = K.layers.MaxPooling2D(pool_size=(2,2))(img_1)
 
-convB = K.layers.Conv2D(name="convBa", filters=64, **params)(poolA)
-convB = K.layers.Conv2D(
-    name="convBb", filters=64, **params)(convB)
-poolB = K.layers.MaxPooling2D(name="poolB", pool_size=(2, 2))(convB)
+img_1 = K.layers.Conv2D(64, **params)((K.layers.BatchNormalization(momentum=bn_momentum))(img_1))
+img_1 = K.layers.MaxPooling2D(name='skip1', pool_size=(2,2))(img_1)
 
-convC = K.layers.Conv2D(name="convCa", filters=32, **params)(poolB)
-convC = K.layers.Conv2D(
-    name="convCb", filters=32, **params)(convC)
-poolC = K.layers.MaxPooling2D(name="poolC", pool_size=(2, 2))(convC)
+# Residual block
+img_2 = K.layers.Conv2D(128, **params) ((K.layers.BatchNormalization(momentum=bn_momentum))(img_1))
+img_2 = K.layers.Conv2D(64, name='img2', **params) ((K.layers.BatchNormalization(momentum=bn_momentum))(img_2))
+img_2 = K.layers.add( [img_1, img_2] )
+img_2 = K.layers.MaxPooling2D(name='skip2', pool_size=(2,2))(img_2)
 
-flat = K.layers.Flatten()(poolC)
+# Residual block
+img_3 = K.layers.Conv2D(128, **params)((K.layers.BatchNormalization(momentum=bn_momentum))(img_2))
+img_3 = K.layers.Conv2D(64, name='img3', **params)((K.layers.BatchNormalization(momentum=bn_momentum))(img_3))
+img_res = K.layers.add( [img_2, img_3] )
 
-drop = K.layers.Dropout(0.5)(flat)
+# Filter residual output
+img_res = K.layers.Conv2D(128, **params)((K.layers.BatchNormalization(momentum=bn_momentum))(img_res))
 
-dense1 = K.layers.Dense(128, activation="relu")(drop)
+# Can you guess why we do this? Hint: Where did Flatten go??
+img_res = K.layers.GlobalMaxPooling2D(name='global_pooling') ( img_res )
 
-dense2 = K.layers.Dense(num_classes, activation="sigmoid")(dense1)
+# What is this? Hint: We have 2 inputs. An image and a number.
+# cnn_out = Concatenate(name='What_happens_here')( [img_res, angle_input] )
 
-model = K.models.Model(inputs=[inputs], outputs=[dense2])
+dense1 = K.layers.Dropout(0.5)(K.layers.Dense(256, activation = "relu")(img_res)) 
+dense2 = K.layers.Dropout(0.5)(K.layers.Dense(64, activation = "relu")(dense1)) 
+dense3 = K.layers.Dense(num_classes, activation = 'sigmoid')(dense2)
 
-opt = K.optimizers.Adam()
+model = K.models.Model(inputs=[inputs], outputs=[dense3])
 
-model.compile(loss=K.losses.categorical_crossentropy,
-              optimizer=opt,
-              metrics=[tf.keras.metrics.CategoricalCrossentropy()])
+opt = K.optimizers.Adam( lr = 1e-3, beta_1 = .9, beta_2 = .999, decay = 1e-3 )
 
-# loss = tf.nn.weighted_cross_entropy_with_logits()
-# https://www.tensorflow.org/api_docs/python/tf/nn/weighted_cross_entropy_with_logits
+model.compile(loss = K.losses.categorical_crossentropy, 
+                optimizer = opt, 
+                metrics = ['accuracy'])
 
+############## Specific Model Definition ENDS here ##############
                                                                  
 model.fit_generator(TRAINING_DATA, 
                     validation_data=VALIDATION_DATA, 
