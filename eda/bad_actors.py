23090571a1bb1358a65e561d33a42110e2b47c99
kshannon <k.shannon.ucsd@gmail.com>
2019-10-11 01:12:51 -0700

    updated bad_actors script to catch nuiscence data we were capturing in the data_loader. Data_loader shall not be concerned with catching bad data.

--- eda/bad_actors.py
+++ eda/bad_actors.py
@@ -7,18 +7,17 @@ import csv
 import numpy as np
 import pandas as pd
 import pydicom
+from multiprocessing import  Pool
 from tqdm.auto import tqdm
 tqdm.pandas()
-
-from multiprocessing import  Pool
 
 
 TRAIN_CSV_PATH = '../src/training.csv'
 VALIDATE_CSV_PATH = '../src/validation.csv'
-TEST_CSV_PATH = '../src/testing.csv'
+# TEST_CSV_PATH = '../src/testing.csv'
 
 train_data = '../../data/stage_1_train_images/'
-test_data = '../../data/stage_1_test_images/'
+# test_data = '../../data/stage_1_test_images/'
 
 
 def check_dicom(row, path=train_data):
@@ -26,20 +25,20 @@ def check_dicom(row, path=train_data):
         data = pydicom.dcmread(path+row[0])
     except ValueError:
         print('corruption on open...')
-        return False
+        return True
     try:
         img = np.array(data.pixel_array, dtype=float)
     except ValueError:
         print('corruption on pixel_array...')
-        return False
+        return True
     if img.shape != (512, 512):
         print('square peg in round hole!')
-        return False
+        return True
     if np.std(img) == 0:
-        print('Zero std dev')
-        return False
+        print('Zero std dev.')
+        return True
 
-    return True
+    return False
 
 
 def parallelize_dataframe(df, func, n_cores=4):
@@ -50,26 +49,64 @@ def parallelize_dataframe(df, func, n_cores=4):
     pool.join()
     return df
 
-def add_features_to_df(df):
-    df['bad_actors'] = df.progress_apply(lambda x: check_dicom(x), axis=1)
 
+def find_bad_actors(df):
+    df['bad_actors'] = df.progress_apply(lambda x: check_dicom(x), axis=1)
     return df
 
-# Comment or Uncomment to process TRAINING data
-df_train = pd.read_csv(TRAIN_CSV_PATH, header=None)
-#df_train = add_features_to_df(df_train)
-df_train = parallelize_dataframe(df_train, add_features_to_df)
-df_train.to_csv('../src/train_flagged.csv', index=False)
 
-# Comment or Uncomment to process VALIDATION data
+############### Comment or Uncomment to process VALIDATION data ###############
 df_validate = pd.read_csv(VALIDATE_CSV_PATH, header=None)
-#df_validate = add_features_to_df(df_validate)
-df_validate = parallelize_dataframe(df_validate, add_features_to_df)
-df_validate.to_csv('../src/validate_flagged.csv', index=False)
+num_rows = df_validate.shape[0]
 
-# Comment or Uncomment to process TEST data
-# df_test = pd.read_csv(TEST_CSV_PATH, header=None)
-# df_test['bad_actors'] = df_test.progress_apply(lambda x: check_dicom(x),axis=1)
-# df_test.to_csv('../src/test_flagged.csv', index=False)
+# find bad actors by reading dicom data
+df_validate = parallelize_dataframe(df_validate, find_bad_actors)
+df_validate_cleaned = df_validate.loc[df_validate['bad_actors'] == False]
+
+# remove bad actors from the df and check shapes
+df_validate_cleaned = df_validate_cleaned.drop(columns=['bad_actors'])
+assert df_validate_cleaned.shape[0] <= df_validate.shape[0]
+
+print("Rows in validate before: ", num_rows)
+print("Rows in validate after: ", df_validate_cleaned.shape[0])
+print("Verified bad actors were removed")
+
+df_validate_cleaned.columns = ["filename", "targets", "any"]
+df_validate_cleaned.to_csv('../src/validation_cleaned.csv', index=False)
+
+
+
+############### Comment or Uncomment to process TRAINING data ###############
+df_train = pd.read_csv(TRAIN_CSV_PATH, header=None)
+train_rows = df_train.shape[0]
+
+# find bad actors by reading dicom data
+df_train = parallelize_dataframe(df_train, find_bad_actors)
+df_train_cleaned = df_train.loc[df_train['bad_actors'] == False]
+
+# remove bad actors from the df and check shapes
+df_train_cleaned = df_train_cleaned.drop(columns=['bad_actors'])
+assert df_train_cleaned.shape[0] <= df_train.shape[0]
+
+print("Rows in train before: ", train_rows)
+print("Rows in train after: ", df_train_cleaned.shape[0])
+print("Verified bad actors were removed")
+
+df_train_cleaned.columns = ["filename", "targets", "any"]
+df_train_cleaned.to_csv('../src/training_cleaned.csv', index=False)
+
+
+
+############### Comment or Uncomment to process TEST data ###############
+# df_validate = pd.read_csv(VALIDATE_CSV_PATH, header=None)
+# print("Rows in train before: ", df_validate.shape[0])
+# df_validate = parallelize_dataframe(df_validate, find_bad_actors)
+# df_validate_cleaned = df_validate.loc[df_validate['bad_actors'] == False]
+# df_validate_cleaned = df_validate_cleaned.drop(columns=['bad_actors'])
+# assert df_validate_cleaned.shape[0] <= df_validate.shape[0]
+# print("Rows in train after: ", df_validate_cleaned.shape[0])
+# os.remove(VALIDATE_CSV_PATH)
+# print("Verified bad actors were removed and deleted old train CSV")
+# df_validate_cleaned.to_csv('../src/training.csv', index=False)
 
 print('All Done!')
