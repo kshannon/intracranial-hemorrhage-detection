4c14cb5de7a824268244d598028030d1efc3f3fe
kshannon <k.shannon.ucsd@gmail.com>
2019-10-22 23:05:35 -0700

    slight bug fix predixt script and add ability to just submit on subtype 'any'

--- src/predict.py
+++ src/predict.py
@@ -13,28 +13,20 @@ import tensorflow as tf
 import data_flow
 from data_loader import DataGenerator
 import parse_config
-from custom_loss import weighted_log_loss, weighted_loss
+# from custom_loss import weighted_log_loss, weighted_loss
 
 
-
-if parse_config.USING_RTX_20XX:
-    config = tf.ConfigProto()
-    config.gpu_options.allow_growth = True
-    tf.keras.backend.set_session(tf.Session(config=config))
+# comment out if using tensorflow 2.x
+# if parse_config.USING_RTX_20XX:
+#     config = tf.ConfigProto()
+#     config.gpu_options.allow_growth = True
+#     tf.keras.backend.set_session(tf.Session(config=config))
 
 MODEL_PATH = '../models/'
 MODEL_EXT = '.pb'
 SUBMISSION_NAME = os.path.join('../submissions/', sys.argv[1]+'-predictions.csv')
 DATA_DIRECTORY = data_flow.TEST_DATA_PATH
 TEST_CSV = "../submissions/phase1_test_filenames.csv"
-
-
-# Load the saved model and test data
-# CUSTOM_OBJECTS = {"weighted_log_loss":weighted_log_loss, "weighted_loss":weighted_loss}
-# MODEL_NAME = os.path.join('../models/', sys.argv[1] + '.pb')
-# MODEL = tf.keras.models.load_model(MODEL_NAME, custom_objects=CUSTOM_OBJECTS)
-
-
 
 # Model names are entered here as a list of strings 
 # e.g. mobilenetv2-dim999x999-bce-oct1v9
@@ -43,7 +35,8 @@ MODELS['epidural'] = []
 MODELS['intraparenchymal'] = []
 MODELS['intraventricular'] = []
 MODELS['subarachnoid'] = []
-MODELS['any'] = ['mobilenetv2-dim244x244-bce-oct22v1']
+MODELS['any'] = ['mobilenetv2-dim224x224-bce-any-oct22v1']
+PREDICT_ONLY_ANY = True #set this to true if you are only loading a model for 'any' subtype
 
 
 BATCH_SIZE = 1
@@ -87,12 +80,13 @@ def predict_on_img(model_array, img, predicting_any=False):
 
 def main():
     # load models
-    epidural_models = load_models("epidural")
-    intraparenchymal_models = load_models("intraparenchymal")
-    intraventricular_models = load_models('intraventricular')
-    subarachnoid_models = load_models('subarachnoid')
-    subdural_models = load_models('subdural')
     any_models = load_models('any')
+    if not PREDICT_ONLY_ANY:
+        epidural_models = load_models("epidural")
+        intraparenchymal_models = load_models("intraparenchymal")
+        intraventricular_models = load_models('intraventricular')
+        subarachnoid_models = load_models('subarachnoid')
+        subdural_models = load_models('subdural')
 
     with open(SUBMISSION_NAME, 'a+', newline='') as outfile:
         writer = csv.writer(outfile)
@@ -104,10 +98,10 @@ def main():
 
 
             # test for subtype any
-            pred_any = predict_on_img(any_models, img)
+            pred_any = predict_on_img(any_models, img, predicting_any=True)
 
             # Handle the case where we predict no occurrence of 'any' IH
-            if pred_any == 0:
+            if pred_any == 0 or PREDICT_ONLY_ANY == True:
                 filename_pred_vector = [0.0, 0.0, 0.0, 0.0, 0.0, pred_any]
                 for filename_pred in zip(INTRACRANIAL_HEMORRHAGE_SUBTYPES, filename_pred_vector):
                     dicom_id = filename[:-4] + "_" + filename_pred[0]
@@ -115,7 +109,7 @@ def main():
                     writer.writerow([dicom_id, filename_pred[1]])
                 continue
             
-            # Handle the case for when we think there is occurance of 'any' IH, lets try to determine the type
+            # Handle the case for when we think there is occurrence of 'any' IH, lets try to determine the type
             if pred_any > 0:
                 pred_epidural = predict_on_img(epidural_models, img)
                 pred_intraparenchymal = predict_on_img(intraparenchymal_models, img)
@@ -133,20 +127,6 @@ def main():
                     # print(readable_id, subtype[1])
                     writer.writerow([dicom_id, filename_pred[1]])
                 continue
-
-        
-
-
-
-
-
-            # print(np.squeeze(prediction))
-        
-            # for subtype in zip(INTRACRANIAL_HEMORRHAGE_SUBTYPES, np.squeeze(prediction)):
-            #     readable_id = filename[:-4] + "_" + subtype[0]
-            #     # print(readable_id, subtype[1])
-            #     writer.writerow([readable_id, subtype[1]])
-
 
 
 if __name__ == "__main__":
